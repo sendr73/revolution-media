62ea48cff134fb49b6e0be8f0c3270fa08ea1e44
Daron Sender <d.sender07@gmail.com>
2024-02-07 19:42:54 +0200

    chore: Initialiasing client structure [F]
    Used Vite to create a React (JS) client file structure. Removed biolerplate code, added dependencies like Tailwind, and creates directories (some empty) for future files

---
+++ client/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: ["./src/**/*.{html,js}"],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
